474147ed6508119d20547b9d1f3ba72908ce6dad
nico0cul <garay.nicolas002@gmail.com>
2022-12-29 16:28:55 -0300

    arreglos

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './componentes/top/top.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
